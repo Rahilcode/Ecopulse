da9b3925cbc04982772185b2b1f678ea2286ebda
Rahilcode <raheelkhatttal@gmail.com>
2023-10-18 18:10:21 +0530

    Implement User auth and user sign up and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "home#home"
 
   get 'home', to: 'home#home'
   get 'about', to: 'home#about'
+
+  get '/user/profile', to: 'home#profile'
 
 end
